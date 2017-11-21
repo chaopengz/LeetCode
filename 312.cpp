4c85543d85061bdbbfea574f4e01901aa56ce8ce
chaopengz <cpz@pku.edu.cn>
2017-11-21 16:16:08 +0800

    update

--- 312.cpp
+++ 312.cpp
@@ -5,8 +5,7 @@
 
 class Solution {
 public:
-    int maxCoins(vector<int> &nums)
-    {
+    int maxCoins(vector<int> &nums) {
         int len = nums.size();
         vector<int> balloons;
         balloons.push_back(1);
@@ -16,10 +15,9 @@ public:
 
         vector<vector<int>> dp(balloons.size(), vector<int>(balloons.size(), 0));
 
-        for (int i = 1; i <= len; ++i)
-        {
-            for (int j = i; j <= len; ++j)
-            {//计算dp[i][j]
+        for (int step = 0; step < len; ++step) {
+            for (int i = 0; i <= len - step; ++i) {
+                int j = i + step;
                 for (int k = i; k <= j; ++k)//k为[i,j]最后一个戳破的气球
                 {
                     dp[i][j] = max(
@@ -29,34 +27,6 @@ public:
                 }
             }
         }
-
         return dp[1][len];
     }
-//    int maxCoins(vector<int>& nums) {
-//        int N = nums.size();
-//        nums.insert(nums.begin(), 1);
-//        nums.insert(nums.end(), 1);
-//
-//        // rangeValues[i][j] is the maximum # of coins that can be obtained
-//        // by popping balloons only in the range [i,j]
-//        vector<vector<int>> rangeValues(nums.size(), vector<int>(nums.size(), 0));
-//
-//        // build up from shorter ranges to longer ranges
-//        for (int len = 1; len <= N; ++len) {
-//            for (int start = 1; start <= N - len + 1; ++start) {
-//                int end = start + len - 1;
-//                // calculate the max # of coins that can be obtained by
-//                // popping balloons only in the range [start,end].
-//                // consider all possible choices of final balloon to pop
-//                int bestCoins = 0;
-//                for (int final = start; final <= end; ++final) {
-//                    int coins = rangeValues[start][final-1] + rangeValues[final+1][end]; // coins from popping subranges
-//                    coins += nums[start-1] * nums[final] * nums[end+1]; // coins from final pop
-//                    if (coins > bestCoins) bestCoins = coins;
-//                }
-//                rangeValues[start][end] = bestCoins;
-//            }
-//        }
-//        return rangeValues[1][N];
-//    }
 };
--- 494.cpp
+++ 494.cpp
@@ -5,20 +5,18 @@
 
 class Solution {
 public:
-    int findTargetSumWays(vector<int> &nums, int S)
-    {
+    int findTargetSumWays(vector<int> &nums, int S) {
         int sum = 0;
-        for(auto i:nums)
+        for (auto i:nums)
             sum += i;
-        int cols = 2*sum+1;
-        if (S > sum || S< -sum)
+        int cols = 2 * sum + 1;
+        if (S > sum || S < -sum)
             return 0;
 
         int dp[20][4005];
         memset(dp, 0, sizeof(dp));
 
-        for (int j = 0; j < cols; ++j)
-        {
+        for (int j = 0; j < cols; ++j) {
             if (!nums[0])//第一个为0,则dp[0][sum]有两个解，正0和负0
                 dp[0][sum] = 2;
             else if (j == nums[0] + sum || j == -nums[0] + sum)
@@ -27,20 +25,17 @@ public:
                 dp[0][j] = 0;
         }
 
-        for (int i = 0; i < nums.size()-1; ++i)
-        {
-            for (int j = 0; j < cols; ++j)
-            {
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            for (int j = 0; j < cols; ++j) {
                 //dp[i][j] = dp[i - 1][j - nums[i]] + dp[i - 1][j + nums[i]];
-                if(dp[i][j])
-                {
-                    dp[i+1][j-nums[i+1]] += dp[i][j];
-                    dp[i+1][j+nums[i+1]] += dp[i][j];
+                if (dp[i][j]) {
+                    dp[i + 1][j - nums[i + 1]] += dp[i][j];
+                    dp[i + 1][j + nums[i + 1]] += dp[i][j];
                 }
             }
         }
 
-        return dp[nums.size() - 1][sum+S];
+        return dp[nums.size() - 1][sum + S];
     }
 };
 
