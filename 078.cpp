a9b559b4e8bc4b20fa3eec5cbaab9033ad419a1b
chaopengz <chaopengz@pku.edu.cn>
2017-11-21 23:00:00 +0800

    add 078

---
+++ 078.cpp
@@ -0,0 +1,17 @@
+//
+// Created by chaopengz on 2017/11/21.
+//
+
+#include "head.h"
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        int len = nums.size();
+        for (int i = 0; i <= len; ++i)
+        {
+
+        }
+    }
+};
