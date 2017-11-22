e5f33c38966906b1fdbd2697c336b9a4fcbeedaa
chaopengz <chaopengz@pku.edu.cn>
2017-11-22 08:12:59 +0800

    add 078

--- 078.cpp
+++ 078.cpp
@@ -9,20 +9,23 @@ public:
     vector<vector<int>> subsets(vector<int> &nums)
     {
         vector<int> v;
+
         vector<vector<int>> ans;
         v.push_back(1);
         v.clear();
         ans.push_back(v);
 
-        for(auto i:nums)
+        for (auto i:nums)
         {
-            for(auto j:ans)
+            vector<vector<int>> pre;
+            pre = ans;
+            for (auto& row:pre)
             {
                 vector<int> k;
-                k = j;
-                for(auto h:k)
-                    cout<<h<<" ";
-                cout<<endl;
+                for (auto& col:row)
+                {
+                    k.push_back(col);
+                }
                 k.push_back(i);
                 ans.push_back(k);
             }
