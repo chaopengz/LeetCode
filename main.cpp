f205c9e2a9092cf46bf227bb92cbed678093c3f1
chaopengz <cpz@pku.edu.cn>
2017-11-22 19:38:31 +0800

    solve 621

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
-#include "head.h"
-#include "621.cpp"
+#include "062.cpp"
 
 int main()
 {
 //    int a[] = {1, 2, 3};
 //    vector<int> v(a, a + 3);
-//
-//    Solution s;
-//    s.subsets(v);
 
-    char a[] = {'A', 'A', 'A', 'B', 'B', 'B'};
-    vector<char> v(a, a + 6);
     Solution s;
-    s.leastInterval(v,2);
+    s.uniquePaths(1,2);
+
+
 }
