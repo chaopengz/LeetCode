2ed056f87410acd3e3dfed0ab76ce43cca164220
chaopengz <cpz@pku.edu.cn>
2017-11-29 18:52:53 +0800

    update

---
+++ 560.cpp
@@ -0,0 +1,26 @@
+//
+// Created by cpz on 2017/11/29.
+//
+#include "head.h"
+
+class Solution {
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int len = nums.size();
+        int ans = 0;
+        int sum;
+
+        for (int i = 1; i <= len; ++i)
+        {
+            sum = 0;
+            for (int j = i; j <= len; ++j)
+            {
+                sum += nums[j - 1];
+                if(sum==k)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
-#include "048.cpp"
+#include "560.cpp"
 
 int main()
 {
-    Solution so;
-    vector<vector<int>> mat;
-    int a[] = {1, 2, 3};
-    int b[] = {4, 5, 6};
-    int c[] = {7, 8, 9};
-    vector<int> v1(a, a + 3), v2(b, b + 3), v3(c, c + 3);
-    mat.push_back(v1);
-    mat.push_back(v2);
-    mat.push_back(v3);
-
-    so.rotate(mat);
+    int a[] = {1, 1, 1};
+    vector<int> v(a, a + 3);
+    Solution s;
+    cout << s.subarraySum(v, 2);
 }
