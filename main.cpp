148a59484aeddf6d1a7121ba7122611aec38e7ec
chaopengz <chaopengz@pku.edu.cn>
2017-11-30 21:03:09 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "040.cpp"
+#include "053.cpp"
 
 int main()
 {
     Solution so;
-//    int a[] = {10, 1, 2, 7, 6, 1, 5};
-//    vector<int> v(a, a + 7);
-    int a[] = {2,3,6,7};
-    vector<int> v(a, a + 4);
-   so.combinationSum2(v, 7);
+    int a[] = {-2,2};
+    vector<int> v(a, a + 1);
+//    int a[] = {-2,1};
+//    vector<int> v(a, a + 2);
+    cout << so.maxSubArray(v);
 }
