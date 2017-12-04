955033f50e50f42c45b9ddb3badeb593f2319a26
chaopengz <chaopengz@pku.edu.cn>
2017-12-05 01:23:06 +0800

    update

--- 240.cpp
+++ 240.cpp
@@ -7,6 +7,17 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>> &matrix, int target)
     {
-
+        if (!matrix.size())
+            return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool res = false;
+        for (int i = 0; i < m; ++i)
+        {
+            res |= binary_search(matrix[i].begin(), matrix[i].end(), target);
+            if (res)
+                break;
+        }
+        return res;
     }
 };
