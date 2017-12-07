d015f13c7913131b8834d85785689f32ed6f135a
chaopengz <chaopengz@pku.edu.cn>
2017-12-07 14:25:11 +0800

    update

--- 049.cpp
+++ 049.cpp
@@ -8,6 +8,33 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
+        string old;
+        vector<vector<string>> ans;
 
+        bool Find;
+        for (auto str:strs)
+        {
+            Find = false;
+            old = str;
+            sort(str.begin(), str.end());
+
+            for (int i = 0; i < ans.size(); ++i)
+            {
+                if (ans[i][0] == str)
+                {
+                    ans[i].push_back(old);
+                    Find = true;
+                    break;
+                }
+            }
+            if (!Find)
+            {
+                vector<string> v;
+                v.push_back(str);
+                v.push_back(old);
+                ans.push_back(v);
+            }
+        }
+        return ans;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-#include "042.cpp"
+#include "049.cpp"
 //#include "buildTree.h"
 
 int main()
 {
     Solution so;
-    int a[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-    vector<int> v(a, a + 12);
+//    int a[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+//    vector<int> v(a, a + 12);
    // sort(v.begin(),v.end());
 
-//    int a[] = {100,4,200,1,3,2};
-//    vector<int> v(a,a+6);
-    cout << so.trap(v);
+    string a[] = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    vector<string> v(a,a+6);
+    so.groupAnagrams(v);
 
 }
