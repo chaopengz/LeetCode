b5a106c10c8fa668b892ef42450172c52c1ecda9
chaopengz <chaopengz@pku.edu.cn>
2017-12-09 00:04:11 +0800

    update

---
+++ 171.cpp
@@ -0,0 +1,15 @@
+//
+// Created by chaopengz on 2017/12/9.
+//
+
+#include "head.h"
+
+class Solution {
+public:
+    int titleToNumber(string s)
+    {
+        map<char, int> m;
+        for (char c = 'A'; c <= 'Z'; c++)
+            m[c] = 97;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -4,13 +4,10 @@
 int main()
 {
     Solution so;
-    BuildTree B;
+
 //    int a[] = {0,1,0,2,1,0,1,3,2,1,2,1};
 //    vector<int> v(a, a + 12);
-    string str = "[1,2,5,3,4,null,6]";
-    TreeNode *root;
-    root = B.stringToTree(str);
-    so.flatten(root);
 
+    cout << 'C' - 'A' + 1 << endl;
 
 }
