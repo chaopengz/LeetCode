45f1e34ea05c87c32dac45a134b3f39837a04017
chaopengz <cpz@pku.edu.cn>
2017-12-11 01:28:55 +0800

    update

--- 454.cpp
+++ 454.cpp
@@ -8,25 +8,26 @@ public:
     int fourSumCount(vector<int> &A, vector<int> &B, vector<int> &C, vector<int> &D)
     {
         int len = A.size();
-        sort(A.begin(), A.end());
-        sort(B.begin(), B.end());
-        sort(C.begin(), C.end());
-        sort(D.begin(), D.end());
-
-        vector<int>::iterator low, up;
+//        sort(A.begin(), A.end());
+//        sort(B.begin(), B.end());
+//        sort(C.begin(), C.end());
+//        sort(D.begin(), D.end());
         int ans = 0;
-        int target;
-        for (int i = 0; i < len; i++)
+        map<int, int> m;
+        for (int i = 0; i < len; ++i)
         {
             for (int j = 0; j < len; ++j)
             {
-                for (int k = 0; k < len; ++k)
-                {
-                    target = 0 - A[i] - B[j] - C[k];
-                    low = lower_bound(D.begin(), D.end(), target);
-                    up = upper_bound(D.begin(), D.end(), target);
-                    ans += (up - low);
-                }
+                int sum = C[i] + D[j];
+                m[-sum]++;
+            }
+        }
+        for (int k = 0; k < len; ++k)
+        {
+            for (int i = 0; i < len; ++i)
+            {
+                int target = A[k] + B[i];
+                ans += m[target];
             }
         }
         return ans;
