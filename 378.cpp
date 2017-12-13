abf77bcbe46e709f178be179b0b4cdf91d915611
chaopengz <cpz@pku.edu.cn>
2017-12-13 14:48:46 +0800

    update

--- 378.cpp
+++ 378.cpp
@@ -4,33 +4,63 @@
 
 #include "head.h"
 
+//class Solution {
+//public:
+//    int kthSmallest(vector<vector<int>> &matrix, int k)
+//    {
+//        int n = matrix.size();
+//
+//        int left = matrix[0][0];
+//        int right = matrix[n - 1][n - 1];
+//        //k最小总在[left,right]中
+//        while (left < right)
+//        {
+//            int midv = (left + right) / 2;
+//
+//            int count = 0;  // number of elements no bigger than midv
+//            for (int i = 0; i < n; i++)
+//            {
+//                vector<int> &row = matrix[i];
+//                count += std::upper_bound(row.begin(), row.end(), midv) - row.begin();
+//            }
+//            //更新left与right边界值
+//            if (count < k)
+//            {
+//                left = midv + 1;
+//            } else//count >= k
+//            {
+//                right = midv;
+//            }
+//        }
+//        return left;
+//    }
+//};
+
 class Solution {
 public:
     int kthSmallest(vector<vector<int>> &matrix, int k)
     {
-        int n = matrix.size();
-
-        int left = matrix[0][0];
-        int right = matrix[n - 1][n - 1];
-        //k最小总在[left,right]中
+        int len = matrix[0].size();
+        int left = matrix[0][0], right = matrix[len - 1][len - 1];
+        int mid = (left + right) / 2;
+        int sum;
+        int count;
         while (left < right)
         {
-            int midv = (left + right) / 2;
-
-            int count = 0;  // number of elements no bigger than midv
-            for (int i = 0; i < n; i++)
+            sum = 0;
+            mid = (left + right) / 2;
+            for (int i = 0; i < len; ++i)
             {
-                vector<int> &row = matrix[i];
-                count += std::upper_bound(row.begin(), row.end(), midv) - row.begin();
+                count = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();// <=
+                if (!count)
+                    break;
+                else
+                    sum += count;
             }
-
-            if (count < k)
-            {
-                left = midv + 1;
-            } else
-            {
-                right = midv;
-            }
+            if (sum < k)
+                left = mid + 1;
+            else
+                right = mid;
         }
         return left;
     }
--- main.cpp
+++ main.cpp
@@ -4,17 +4,21 @@
 int main()
 {
     Solution so;
-    int a[] = {1, 5, 9};
-    int b[] = {10, 11, 13};
-    int c[] = {12, 13, 15};
-    vector<int> v1(a, a + 3);
-    vector<int> v2(b, b + 3);
-    vector<int> v3(c, c + 3);
-    vector<vector<int>> v;
-    v.push_back(v1);
-    v.push_back(v2);
-    v.push_back(v3);
-
-    so.kthSmallest(v,8);
+//    int a[] = {1, 5, 9};
+//    int b[] = {10, 11, 13};
+//    int c[] = {12, 13, 15};
+//    vector<int> v1(a, a + 3);
+//    vector<int> v2(b, b + 3);
+//    vector<int> v3(c, c + 3);
+//    vector<vector<int>> v;
+//    v.push_back(v1);
+//    v.push_back(v2);
+//    v.push_back(v3);
+    vector<vector<int>> vv;
+    vector<int> v;
+    v.push_back(2000000000);
+    //v.push_back(-5);
+    vv.push_back(v);
+    so.kthSmallest(vv, 1);
 
 }
