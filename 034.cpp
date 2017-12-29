86d68389eebe6740d2f486f5f087aafe847950d8
chaopengz <cpz@pku.edu.cn>
2017-12-30 01:14:32 +0800

    update

--- 034.cpp
+++ 034.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "head.h"
+
 // Using STL
 //class Solution {
 //public:
@@ -25,5 +26,67 @@
 //        return ans;
 //    }
 //};
+class Solution {
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+        vector<int> ans;
+        int low = 0, high = nums.size() - 1;
+        int mid;
+        int ans1 = -1, ans2 = -1;
+        while (low <= high)//找最低
+        {
+            mid = (low + high) / 2;
+            if (lowMatch(nums, target, mid))
+            {
+                ans1 = mid;
+                break;
+            } else if (nums[mid] < target)
+                low = mid + 1;
+            else// nums[mid]>=target
+                high = mid - 1;//即使相等，也一定在左边找
+        }
 
+        low = 0, high = nums.size() - 1;
+
+        while (low <= high)//找最高
+        {
+            mid = (low + high) / 2;
+            if (highMatch(nums, target, mid))
+            {
+                ans2 = mid;
+                break;
+            } else if (nums[mid] <= target)//由上一个条件，即使跟target相等也不符合了
+                low = mid + 1;
+            else //nums[mid]>target
+                high = mid - 1;//mid有可能是解
+        }
+
+        ans.push_back(ans1);
+        ans.push_back(ans2);
+        return ans;
+    }
+
+    bool lowMatch(vector<int> &nums, int target, int i)
+    {
+        if (nums[i] != target)
+            return false;
+        if (i == 0)//第0个肯定是最左边的
+            return true;
+        else if (nums[i] != nums[i - 1])
+            return true;
+        return false;
+    }
+
+    bool highMatch(vector<int> &nums, int target, int i)
+    {
+        if (nums[i] != target)
+            return false;
+        if (i == nums.size() - 1)//最后一个肯定是最右边的
+            return true;
+        else if (nums[i] != nums[i + 1])
+            return true;
+        return false;
+    }
+};
 
--- main.cpp
+++ main.cpp
@@ -3,5 +3,13 @@
 
 int main()
 {
-
+    int a[] = {2, 2};
+    vector<int> v(a, a + 2);
+    Solution solution;
+    vector<int> ans;
+    ans = solution.searchRange(v, 2);
+    for (auto item:ans)
+    {
+        cout << item << " ";
+    }
 }
