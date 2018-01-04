ddec9bb790d16bd18b3c63120231cc012d5f9d56
chaopengz <cpz@pku.edu.cn>
2018-01-04 16:35:29 +0800

    update

--- 154.cpp
+++ 154.cpp
@@ -3,58 +3,69 @@
 //
 #include "head.h"
 
+//
+//class Solution {
+//public:
+//    int findMin(vector<int> &rotateArray)
+//    {
+//        if (!rotateArray.size())
+//            return 0;
+//        if (rotateArray.size() == 1)
+//            return rotateArray[0];
+//        int len = rotateArray.size();
+//
+//        if (rotateArray[0] < rotateArray[len - 1])//没有旋转
+//            return rotateArray[0];
+//
+//
+//        int low = 0, high = len - 1;
+//        int mid;
+//
+//        while (low <= high)
+//        {
+//            mid = (low + high) / 2;
+//            if (isFind(rotateArray, mid))
+//                return rotateArray[mid];
+//            else if (rotateArray[mid] > rotateArray[0])//[0,mid]单调增
+//                low = mid + 1;
+//            else
+//                high = mid - 1;
+//        }
+//        return rotateArray[mid];
+//    }
+//
+//    bool isFind(vector<int> nums, int index)
+//    {
+//        if (index == nums.size() - 1)
+//            return nums[index] < nums[index - 1];
+//        else if (index == 0)
+//            return nums[index] < nums[nums.size() - 1];
+//        else
+//            return nums[index] <= nums[index + 1] && nums[index] < nums[index - 1];
+//    }
+//};
 class Solution {
 public:
-    int findMin(vector<int> &rotateArray)
+    int findMin(vector<int> &array)
     {
-        if (!rotateArray.size())
-            return 0;
-        if (rotateArray.size() == 1)
-            return rotateArray[0];
-        int len = rotateArray.size();
-
-        if (rotateArray[0] < rotateArray[len - 1])//没有旋转
-            return rotateArray[0];
-
-
-        int low = 0, high = len - 1;
-        int mid;
-
-        while (low <= high)
-        {
-            mid = (low + high) / 2;
-            if (isFind(rotateArray, mid))
-                return rotateArray[mid];
-            else if (rotateArray[mid] >= rotateArray[0])//[0,mid]单调增
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-        return 0;
-    }
-
-    bool isFind(vector<int> nums, int index)
-    {
-        if (index == nums.size() - 1)
-            return nums[index] < nums[index - 1];
-        else if (index == 0)
-            return nums[index] < nums[nums.size() - 1];
-        else
-            return nums[index] <= nums[index + 1] && nums[index] < nums[index - 1];
-    }
-
-    bool findMax(vector<int> nums)
-    {
-        int low = 0, high = nums.size();
+        int low = 0;
+        int high = array.size() - 1;
         int mid;
         while (low < high)
         {
-            mid = (low + high) / 2;
-
+            mid = low + (high - low) / 2;
+            if (array[mid] > array[high])//最小值一定在mid右边
+            {
+                low = mid + 1;
+            } else if (array[mid] == array[high])//最小值有可能在mid左边,也有可能在右边
+            {
+                high = high - 1;//去掉重复的
+            } else//array[mid]<array[high] => mid右边是单调,mid是右边最小得数,也可能是左边最小的数,所以high = mid,而非high = mid-1
+            {
+                high = mid;
+            }
         }
-        return nums[low];
+        return array[low];
     }
-
 };
-
 
--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 
 int main()
 {
-    int a[] = {1, 1, 1};
-    vector<int> v(a, a + 3);
+//    int a[] = {3, 4, 4, 5, 1, 1, 2, 2};
+//    int a[] = {10, 1, 10, 10, 10};
+    int a[] = {8, 8, 8, 8, 8, 8, 1, 8, 8};
+//    int a[] = {8, 8, 1, 8, 8, 8, 8, 8, 8};
+    vector<int> v(a, a + 9);
+
+
     Solution solution;
     cout << solution.findMin(v);
 }
