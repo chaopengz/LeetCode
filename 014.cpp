a7d1a452dc79abfb15f0c02ffe9b4b0a55a844d8
cpz <cpz@pku.edu.cn>
2018-02-12 19:42:15 +0800

    update

--- 014.cpp
+++ 014.cpp
@@ -13,15 +13,31 @@ public:
         int minLen = INT32_MAX, len;
         for (int i = 0; i < strs.size(); ++i)
         {
-            len = strs[i].size();
+            len = (int)strs[i].size();
             minLen = min(minLen, len);
         }
         int index = 0;
+        bool equal;
         while (index < minLen)
         {
-
-            index++;
+            equal = true;
+            for (int i = 1; i < strs.size(); ++i)
+            {
+                if (strs[i][index] != strs[i - 1][index])
+                {
+                    equal = false;
+                }
+            }
+            if (equal)
+                index++;
+            else
+                break;
         }
-
+        string ans = "";
+        for (int j = 0; j < index; ++j)
+        {
+            ans += strs[0][j];
+        }
+        return ans;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,51 +1,15 @@
-//#include "114.cpp"
-//#include "buildTree.h"
-//
-//int main()
-//{
-//    Solution solution;
-//    BuildTree buildTree;
-//    TreeNode *root = buildTree.stringToTree("[1,2,5,3,4,null,6]");
-//    solution.flatten(root);
-//
-//}
-#include<iostream>
-using namespace std;
+#include "014.cpp"
+#include "buildTree.h"
 
-class A
+int main()
 {
-public:
-    void foo()
-    {
-        printf("1\n");
-    }
-    virtual void fun()
-    {
-        printf("2\n");
-    }
-};
-class B : public A
-{
-public:
-    void foo()
-    {
-        printf("3\n");
-    }
-    void fun()
-    {
-        printf("4\n");
-    }
-};
+    Solution solution;
+    vector<string> v;
+    v.push_back("abcf");
+    v.push_back("abcw");
+    v.push_back("abce");
+    v.push_back("");
 
-int main(void)
-{
-    A a;
-    B b;
-    A *p = &a;
-    p->foo();
-    p->fun();
-    p = &b;
-    p->foo();
-    p->fun();
-    return 0;
+    cout << solution.longestCommonPrefix(v);
+
 }
