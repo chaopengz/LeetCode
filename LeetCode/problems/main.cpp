b9af5e0efc7c95fa4bcc84e2cc9222436a60496d
laryzhang(张潮鹏) <laryzhang@tencent.com>
2019-04-12 16:18:49 +0800

    update

--- LeetCode/problems/main.cpp
+++ LeetCode/problems/main.cpp
@@ -6,5 +6,5 @@ int main()
     vector<int> in = {9, 3, 15, 20, 7};
     vector<int> postorder = {9, 15, 7, 20, 3};
     TreeNode *root = s.buildTree(in, postorder);
-    std::cout << "hello world";
+    std::cout << "hello world1";
 }
